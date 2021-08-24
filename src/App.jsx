684af6edff2ab28740a779837e9f497a5b84ebae
HamazakiHisanori <81221219+HamazakiHisanori@users.noreply.github.com>
2021-08-24 15:41:05 +0900

    useStateとuseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert("A");
+  const [num, setNum] = useState(0);
+  const [faceShow, setFaceShow] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwithFaceShow = () => {
+    setFaceShow(!faceShow);
+  };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>count</button>
+      <p>{num}</p>
+      <button onClick={onClickSwithFaceShow}>切り替え</button>
+      {faceShow && <p>٩( ᐛ )و</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color
+    color
   };
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
